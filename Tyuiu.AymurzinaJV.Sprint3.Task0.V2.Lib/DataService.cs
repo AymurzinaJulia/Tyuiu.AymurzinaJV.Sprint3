aab867ca5f0f45f71a209f3456955ffe243d0dae
AymurzinaJulia <aymurzinajulia@gmail.com>
2025-10-30 19:39:43 +0500

    Add project files.

---
+++ Tyuiu.AymurzinaJV.Sprint3.Task0.V2.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.AymurzinaJV.Sprint3.Task0.V2.Lib
+{
+    public class DataService : ISprint3Task0V2
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double constsin = Math.Sin(1);
+            double sinpow = Math.Pow(constsin, -7);
+
+            double res = 1.0;
+
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                double a = Math.Pow(i / sinpow, -2);
+                res *= a;
+            }
+
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.AymurzinaJV.Sprint3.Task0.V2.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.AymurzinaJV.Sprint3.Task0.V2.Lib;
+namespace Tyuiu.AymurzinaJV.Sprint3.Task0.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(0.021, ds.GetMultiplySeries(1, 9));
+        }
+    }
+}
