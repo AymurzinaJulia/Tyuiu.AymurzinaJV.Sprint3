29cafdc9bbf542e09ca76a9ef31eac3f47dfec23
AymurzinaJulia <aymurzinajulia@gmail.com>
2025-10-30 21:56:44 +0500

    Task1 added

---
+++ Tyuiu.AymurzinaJV.Sprint3.Task1.V16.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.AymurzinaJV.Sprint3.Task1.V16.Lib;
+namespace Tyuiu.AymurzinaJV.Sprint3.Task1.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double res = ds.GetSumSeries(1, 1, 15);
+            double wait = 16.936;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
