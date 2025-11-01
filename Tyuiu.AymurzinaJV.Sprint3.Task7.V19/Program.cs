db774f6b1b59ab67ae9862ddc1598c0da703345d
AymurzinaJulia <aymurzinajulia@gmail.com>
2025-11-01 14:00:36 +0500

    Task7 added

--- Tyuiu.AymurzinaJV.Sprint3.Task7.V19/Program.cs
+++ Tyuiu.AymurzinaJV.Sprint3.Task7.V19/Program.cs
@@ -19,16 +19,28 @@ Console.WriteLine("*************************************************************
 int startValue = -5;
 int stopValue = 5;
 
+Console.WriteLine(" Старт шага = " + startValue);
+Console.WriteLine(" Конец шага = " + stopValue);
+
+int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+double[] valueArray;
+valueArray = new double[len];
+
+valueArray = ds.GetMassFunction(startValue, stopValue);
 
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
 Console.WriteLine("***************************************************************************");
 
-var res = ds.GetMassFunction(startValue, stopValue);
+Console.WriteLine("+----------+----------+");
+Console.WriteLine("|    X     |    F(x)  |");
+Console.WriteLine("+----------+----------+");
 
-foreach (var item in res)
+for (int i = 0; i <= len - 1; i++)
 {
-    Console.WriteLine(item);
+    Console.WriteLine("|{0,5:d}     |  {1, 5:f2}   |", startValue, valueArray[i]);
+    startValue++;
 }
-
+Console.WriteLine("+----------+----------+");
 Console.ReadKey();
